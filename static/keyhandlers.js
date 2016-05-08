3f28132467ce294b9a8242e55778bcc78f52092a
aeTIos <aetios@codewalr.us>
2016-05-08 22:00:12 +0200

    changed keybindings, prettified code

--- static/keyhandlers.js
+++ static/keyhandlers.js
@@ -1,18 +1,22 @@
-document.addEventListener('keydown', function(event) {
-    if(event.keyCode == 37) {
-       var xhr = new XMLHttpRequest();
-       xhr.timeout = 2000;
-       xhr.open("get", "/nexturl", true);
-       xhr.send();
-       xhr.responseType = 'text';
-       xhr.onload = function(){
-	       console.log(xhr.response);
-	       console.log("success");
-		var pic = document.getElementById("moepicture");
-		pic.src = xhr.response;
-		}
-}
-    else if(event.keyCode == 39) {
-        alert('Right was pressed');
+document.addEventListener('keydown', function(event) 
+{
+    if(event.keyCode == 82) // key r
+    {
+        var xhr = new XMLHttpRequest();
+        xhr.timeout = 2000;
+        xhr.open("get", "/random", true);
+        xhr.send();
+        xhr.responseType = 'text';
+        xhr.onload = function()
+        {
+            console.log(xhr.response);;
+            var pic = document.getElementById("moepicture")            
+            pic.src = xhr.response;
+        }
     }
-});
+    else if(event.keyCode == 39) 
+    {
+     //   alert('Right was pressed');
+    }
+}
+);
